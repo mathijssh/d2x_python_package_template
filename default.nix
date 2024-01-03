dab815d7f676e7116bfdc96b4a6a19178c6db6da
mathijssh <mathijssh@proton.me>
2024-01-03 18:10:01 +0100

    Initial commit

---
+++ default.nix
@@ -0,0 +1,41 @@
+# An example package with dependencies defined via pyproject.toml
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.pip
+  ];
+
+  deps = {nixpkgs, ...}: {
+    #python = nixpkgs.python310;
+    python = nixpkgs.${pyproject.tool.dream2nix.python-version};
+  };
+
+  inherit (pyproject.project) name version;
+
+  mkDerivation = {
+    src = ./.;
+  };
+
+  buildPythonPackage = {
+    format = lib.mkForce "pyproject";
+    pythonImportsCheck = [
+      #pyproject.tool.dream2nix.project-folder 
+      "my_tool"
+    ];
+  };
+
+  pip = {
+    pypiSnapshotDate = pyproject.tool.dream2nix.pypi-snapshot-date;
+    requirementsList =
+      pyproject.build-system.requires
+      or []
+      ++ pyproject.project.dependencies;
+    flattenDependencies = true;
+  };
+}
